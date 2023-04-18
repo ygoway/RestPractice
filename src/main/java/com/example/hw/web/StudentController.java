ab3ccaa3c1326e7f0ab11a01abcb07fb6367678a
Andrii <velandre375@gmail.com>
2023-04-18 20:41:18 +0300

    change return DTO obj/ no table conflict of get controllers

--- src/main/java/com/example/hw/web/StudentController.java
+++ src/main/java/com/example/hw/web/StudentController.java
@@ -1,7 +1,6 @@
 package com.example.hw.web;
 
 import com.example.hw.dto.StudentDto;
-import com.example.hw.repository.entity.Student;
 import com.example.hw.service.DtoValidation;
 import com.example.hw.service.StudentService;
 import lombok.RequiredArgsConstructor;
@@ -28,19 +27,18 @@ public class StudentController {
         if (bindingResult.hasErrors()) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(dtoValidation.inputDtoValidation(bindingResult));
         }
-        studentService.createStudent(studentDto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(studentDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(studentService.createStudent(studentDto));
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/{studentId}")
     public ResponseEntity getStudentById(@PathVariable Long studentId) {
-        Student student = studentService.getStudentById(studentId);
+        StudentDto student = studentService.getStudentById(studentId);
         return ResponseEntity.ok(student);
     }
 
     @GetMapping
     public ResponseEntity getAllStudents() {
-        List<Student> students = studentService.getAllStudents();
+        List<StudentDto> students = studentService.getAllStudents();
         return ResponseEntity.ok(students);
     }
 
